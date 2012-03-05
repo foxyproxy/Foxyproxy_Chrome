227ce3d25f35ff0cad69395b3d3ff9ab1ca081c3
jhansen <jhansen@546c75aa-6cba-41ed-a450-2a12cba78a36>
2012-03-05 17:13:47 +0000

    git-svn-id: https://svn.getfoxyproxy.org/chrome/trunk@15 546c75aa-6cba-41ed-a450-2a12cba78a36

--- ChromeReload/build_x64_x86.bat
+++ ChromeReload/build_x64_x86.bat
@@ -1,3 +1,3 @@
-""C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"" --no-message-box --pack-extension="%cd%\Basic" --pack-extension-key="%cd%\Basic.pem" 
-""C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"" --no-message-box --pack-extension="%cd%\Standard" --pack-extension-key="%cd%\Standard.pem" 
+#"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --no-message-box --pack-extension="%cd%\Basic" --pack-extension-key="%cd%\Basic.pem" 
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --no-message-box --pack-extension="%cd%\Standard" --pack-extension-key="%cd%\Standard.pem" 
 
