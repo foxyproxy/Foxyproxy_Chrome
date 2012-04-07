cf5863dd862ac9a0c4bf591dc21f4a38cdcfa817
ejung <ejung@546c75aa-6cba-41ed-a450-2a12cba78a36>
2012-04-07 19:27:16 +0000

    Removing crufty original version.
    git-svn-id: https://svn.getfoxyproxy.org/chrome/trunk@20 546c75aa-6cba-41ed-a450-2a12cba78a36

--- ChromeReload/build_x64_x86.bat
+++
@@ -1,3 +0,0 @@
-#"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --no-message-box --pack-extension="%cd%\Basic" --pack-extension-key="%cd%\Basic.pem" 
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --no-message-box --pack-extension="%cd%\Standard" --pack-extension-key="%cd%\Standard.pem" 
-
