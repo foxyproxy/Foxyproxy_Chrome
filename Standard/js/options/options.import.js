fc3f999057276bbb9cbedf7f509232dd7bbecf7c
lduros <lduros@546c75aa-6cba-41ed-a450-2a12cba78a36>
2012-11-07 15:49:27 +0000

    working FileReader implementation
    git-svn-id: https://svn.getfoxyproxy.org/chrome/trunk@103 546c75aa-6cba-41ed-a450-2a12cba78a36

--- Standard/js/options/options.import.js
+++ Standard/js/options/options.import.js
@@ -1,23 +1,18 @@
 // see: http://www.html5rocks.com/en/tutorials/file/filesystem/#toc-support
 // and: http://www.html5rocks.com/en/tutorials/file/xhr2/
-window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+window.onload = function () {
+    window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
 
-var successFileSystem = function (fs) {
-    console.log("FileSystem available", fs.name);
-    fs.root.getFile("log.txt", {}, function (fileEntry) {
-        fileEntry.file(function(file) {
-            var reader = new FileReader();
-            reader.onloadend = function (e) {
-                var txtArea = document.createElement('textarea');
-                txtArea.value = this.result;
-                document.body.getElementsById("pageGlobal").appendChild(txtArea);
-            };
-            reader.readAsText(file);
-        }, errorHandler);
-    }, errorHandler);
-};
-var errorFileSystem = function () {
-    console.log("An error occurred");
-};
+    var fileInput = document.querySelector("#settings-import");
+    var file;
 
-window.requestFileSystem(window.TEMPORARY, 5*1024*1024, successFileSystem, errorFileSystem);
+    fileInput.onchange = function(e){
+        file = e.target.files[0];
+        var reader = new FileReader();      
+        reader.onload = function (event) {
+            console.log(event.target.result);  //contains the file data
+
+        };      
+        reader.readAsText(file);
+    };
+};
