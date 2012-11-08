61278b836df7604378556a907701537fa3234f7a
lduros <lduros@546c75aa-6cba-41ed-a450-2a12cba78a36>
2012-11-08 16:08:25 +0000

    Done with pattern parsing
    git-svn-id: https://svn.getfoxyproxy.org/chrome/trunk@106 546c75aa-6cba-41ed-a450-2a12cba78a36

--- Standard/js/options/options.import.js
+++ Standard/js/options/options.import.js
@@ -1,14 +1,111 @@
 // see: http://www.html5rocks.com/en/tutorials/file/filesystem/#toc-support
 // and: http://www.html5rocks.com/en/tutorials/file/xhr2/
 
-var checkBoolean = function (str) {
+var checkBoolean = function (str, defaultNum) {
     if (str == "true") {
         return true;
     }
     
-    return false;
+    return defaultNum;
 
-}
+};
+
+var checkNumeric = function (str) {
+    if ($.isNumeric(str)) {
+        return str;
+    }
+    return 0;
+};
+
+var checkValidType = function (str) {
+    if (str === "direct" ||
+        str === "manual" ||
+        str === "auto") {
+        return str;
+    }
+    return "direct";
+};
+
+var importPatterns = function (xmlProxy, proxy, xmlDoc) {
+    try {
+        console.log("trying import patterns for", proxy.data.name);
+        var xmlPatterns = xmlProxy.getElementsByTagName("matches")[0].getElementsByTagName("match");
+        var i = 0, length = xmlPatterns.length, pattern;
+        
+        var patterns = [];
+
+        for (; i < length; i++) {
+            console.log('running through patterns');
+            pattern =  new ProxyPattern();
+            pattern.data.enabled = checkBoolean(xmlPatterns[i].getAttribute("enabled"), false);
+            pattern.data.temp = false;
+            pattern.data.name = xmlPatterns[i].getAttribute("name");
+            pattern.data.url = xmlPatterns[i].getAttribute("pattern");
+            console.log("isRegexp for ", pattern.data.name, "is", xmlPatterns[i].getAttribute("isRegEx"));
+            pattern.data.type = (xmlPatterns[i].getAttribute("isRegEx") == "true") ? "regexp" : "wildcard";
+            pattern.data.whitelist = (xmlPatterns[i].getAttribute("isBlackList") == "true") ? "Exclusive" : "Inclusive";
+            patterns.push(pattern);
+        }
+        proxy.data.patterns = patterns;
+    } catch (e) {
+        // no parsable pattern... just forget about it.
+        return;
+    }
+
+};
+
+var importProxies = function (xmlDoc) {
+    var proxy, elem, patterns;
+    var i = 0;
+    var proxies = xmlDoc.getElementsByTagName("proxies")[0].getElementsByTagName("proxy");
+    
+    for (; i < proxies.length; i++) {
+        
+
+        /* rebuild a proxy from xml element */
+        proxy = new Proxy();
+
+        proxy.data.type = checkValidType(proxies[i].getAttribute("mode"));
+        proxy.data.name = proxies[i].getAttribute("name");
+        proxy.data.notes = proxies[i].getAttribute("notes");
+
+        if (proxy.data.name != 'Default' && proxy.data.type != 'direct') {
+
+            proxy.data.enabled = checkBoolean(proxies[i].getAttribute("enabled"));
+            proxy.data.cycle = checkBoolean(proxies[i].getAttribute("includeInCycle"));
+            proxy.data.useDns = checkBoolean(proxies[i].getAttribute("proxyDNS"));
+
+            if (proxies[i].getElementsByTagName('manualconf')[0] != undefined) {
+                elem = proxies[i].getElementsByTagName('manualconf')[0];
+                proxy.data.isSocks = checkBoolean(elem.getAttribute("isSocks"));
+                proxy.data.socks = checkNumeric(elem.getAttribute("socksversion"), 5);
+                proxy.data.host = elem.getAttribute("host");
+                proxy.data.port = checkNumeric(elem.getAttribute("port"), 0);
+            } else {
+                proxy.data.isSocks = false;
+                proxy.data.socks = 5;
+            }
+            
+            proxy.data.notifOnLoad = true;
+            proxy.data.notifOnError = true;
+
+            if (proxies[i].getElementsByTagName('autoconf')[0] != undefined) {
+                elem = proxies[i].getElementsByTagName('autoconf')[0];
+                proxy.data.reloadPAC = checkBoolean(elem.getAttribute('autoReload'));
+                proxy.data.configUrl = elem.getAttribute("url");
+            } else {
+                proxy.data.reloadPAC = false;
+            }
+
+            /* handle patterns */
+            importPatterns(proxies[i], proxy, xmlDoc);
+
+            list.splice(0, 0, proxy);
+        }
+    }
+};
+
+
 
 window.onload = function () {
     window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
@@ -22,51 +119,9 @@ window.onload = function () {
         reader.onload = function (event) {
             try {
                 var xmlDoc = $.parseXML(event.target.result);
-
-                var proxies = xmlDoc.getElementsByTagName("proxies")[0].getElementsByTagName("proxy");
-
+                
                 var addProxies = function () {
-                    var proxy, elem;
-                    var i = 0;
-                    for (; i < proxies.length; i++) {
-                        /* rebuild a proxy from xml element */
-                        proxy = new Proxy();
-
-                        proxy.data.type = proxies[i].getAttribute("mode");
-                        proxy.data.name = proxies[i].getAttribute("name");
-                        proxy.data.notes = proxies[i].getAttribute("notes");
-
-                        if (proxy.data.name != 'Default') {
-                            console.log("adding", proxy.data.name);
-                            proxy.data.enabled = checkBoolean(proxies[i].getAttribute("enabled"));
-                            proxy.data.cycle = checkBoolean(proxies[i].getAttribute("includeInCycle"));
-                            proxy.data.useDns = checkBoolean(proxies[i].getAttribute("proxyDNS"));
-
-                            if (proxies[i].getElementsByTagName('manualconf')[0] != undefined) {
-                                elem = proxies[i].getElementsByTagName('manualconf')[0];
-                                proxy.data.isSocks = checkBoolean(elem.getAttribute("isSocks"));
-                                proxy.data.socks = elem.getAttribute("socksversion");
-                                proxy.data.host = elem.getAttribute("host");
-                                proxy.data.port = elem.getAttribute("port");
-                            } else {
-                                proxy.data.isSocks = false;
-                                proxy.data.socks = 5;
-                            }
-                            
-                            proxy.data.notifOnLoad = true;
-                            proxy.data.notifOnError = true;
-
-                            if (proxies[i].getElementsByTagName('autconf')[0] != undefined) {
-                                elem = proxy.data.getElementsByTagName('autoconf')[0];
-                                proxy.data.reloadPAC = checkBoolean(elem.getAttribute('autoReload'));
-                                proxy.data.configUrl = elem.getAttribute("url");
-                            } else {
-                                proxy.data.reloadPAC = false;
-                            }
-                            list.splice(0, 0, proxy);
-                        }
-                    }
-
+                    importProxies(xmlDoc);
                     updateProxyTable();
                     saveProxies();
                     
@@ -97,7 +152,6 @@ window.onload = function () {
                                      
                                     });
 
-                console.log("List of existing proxies, count", list);
                 $("#import-result").text(localize("Your FoxyProxy Settings file has been imported successfully."));
             } catch (e) {
                 $("#import-result").text(localize("An error occurred. The file you have chosen may not contain valid FoxyProxy settings. Please try to use a different file."));
