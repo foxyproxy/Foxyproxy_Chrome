7c5949d5f7b2d10523383fbaf463d30ea245aff4
lduros <lduros@546c75aa-6cba-41ed-a450-2a12cba78a36>
2013-01-14 15:16:25 +0000

    now using shift key
    git-svn-id: https://svn.getfoxyproxy.org/chrome/trunk@111 546c75aa-6cba-41ed-a450-2a12cba78a36

--- Standard/js/content.js
+++ Standard/js/content.js
@@ -1,7 +1,7 @@
 $(document.body).keydown(function (e) {
 
 	if((e.altKey && e.keyCode == 113 && !e.ctrlKey) 
-           || (e.altKey && e.ctrlKey && e.keyCode && String.fromCharCode(e.keyCode).toUpperCase() == 'A')){
+           || (e.shiftKey && e.ctrlKey && e.keyCode && String.fromCharCode(e.keyCode).toUpperCase() == 'A')){
 
 	    chrome.extension.sendRequest({
 		action:'addpattern', 
