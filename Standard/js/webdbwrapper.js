d535378dc598fecee026f5262aa2b777af0bfd49
lduros <lduros@546c75aa-6cba-41ed-a450-2a12cba78a36>
2013-01-14 15:35:55 +0000

    removing cruft.
    git-svn-id: https://svn.getfoxyproxy.org/chrome/trunk@112 546c75aa-6cba-41ed-a450-2a12cba78a36

--- Standard/js/webdbwrapper.js
+++ Standard/js/webdbwrapper.js
@@ -1,13 +1,13 @@
 function Database(_options){
 	//Database openDatabase(in DOMString name, in DOMString version, in DOMString displayName, in unsigned long estimatedSize, in optional DatabaseCallback creationCallback);
-	console.log(_options)
+	console.log(_options);
 	var options = $.extend(null, {
 		name: 'database',
 		version: "0.1",
 		displayName: "database",
 		estimatedSize: 5*1024*1024,
 		onOpen: null
-	}, _options)
+	}, _options);
 	
 	this.db = openDatabase(options.name, options.version, options.displayName, options.estimatedSize, options.creationCallback);
 	console.log(this.db);
@@ -20,34 +20,34 @@ Database.prototype.createTable = function(_param){
 		callback: null
 	}, _param);
 	this.db.transaction(function (t) {
-		console.log(t)
+		console.log(t);
 		t.executeSql('CREATE TABLE IF NOT EXISTS '+param.name+' ('+param.fields.toString()+')');
 	});
-}
+};
 
 Database.prototype.clearTable = function(tableName, callback){
 	this.db.transaction(function (t) {
 		t.executeSql('DELETE FROM '+tableName, null, callback);
 	});
-}
+};
 
 Database.prototype.addRow = function(_param, callback){
 	var self = this;
 	var param = $.extend(null, {
 		table: 'table',
-		params: [],
+		params: []
 	}, _param);
 	var vals = '';
 
 	this.db.transaction(function (t) {
-		t.executeSql("INSERT INTO " +param.table + " VALUES ("+$.map(param.params, function(el){return "?"}).join(",")+")", param.params, callback);
+		t.executeSql("INSERT INTO " +param.table + " VALUES ("+$.map(param.params, function(el){return "?";}).join(",")+")", param.params, callback);
 	});
-}
+};
 
 Database.prototype.removeRow = function(_param, callback){
 	var param = $.extend(null, {
 		table: 'table',
-		params: [],
+		params: []
 	}, _param);
 	var names = [];
 	var vals = [];
@@ -60,18 +60,18 @@ Database.prototype.removeRow = function(_param, callback){
 	this.db.transaction(function (t) {
 		t.executeSql("DELETE FROM " +param.table + " WHERE "+sqlParams, vals ,callback);
 	});
-}
+};
 
 Database.prototype.getRows = function(_param, callback){
 	var param = $.extend(null, {
 		sql: 'select * frome table',
-		params: [],
+		params: []
 	}, _param);
 	
 	this.db.transaction(function (t) {
 		t.executeSql(param.sql, param.params, callback);
 	});
-}
+};
 
 Database.prototype.truncateTable = function(_param, callback){
 	var param = $.extend(null, {
@@ -84,4 +84,4 @@ Database.prototype.truncateTable = function(_param, callback){
 		t.executeSql("DELETE FROM "+param.table+" WHERE timestamp NOT IN (SELECT timestamp FROM "+param.table+" ORDER BY timestamp LIMIT 0, ?)", [param.size], callback);
 	});
 
-}
+};
