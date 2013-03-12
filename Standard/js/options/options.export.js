dce972fd70bd4efdaff35ba7f0e0125c1a0d2cb9
lduros <lduros@546c75aa-6cba-41ed-a450-2a12cba78a36>
2013-03-12 01:41:14 +0000

    finished export, but bug with Firefox needs to be sorted out
    git-svn-id: https://svn.getfoxyproxy.org/chrome/trunk@124 546c75aa-6cba-41ed-a450-2a12cba78a36

--- Standard/js/options/options.export.js
+++ Standard/js/options/options.export.js
@@ -1,118 +1,220 @@
 var generateXMLForPatterns = function (xmlDoc, patternsList) {
-    var j = 0, patternLen, curPattern, curPatternNode, matches, match;
-    console.log("generateXMLForPatterns called");
-    if (patternsList) {
-        matches = xmlDoc.createElement("matches");
+  var j = 0, patternLen, curPattern, curPatternNode, matches, match;
+  
+  if (patternsList) {
+    matches = xmlDoc.createElement("matches");
 
-        for (j = 0, patternLen = patternsList.length; j < patternLen; j++) {
-            curPattern = patternsList[j].data;
-            match = xmlDoc.createElement("match");
-            match.setAttribute("enabled", curPattern.enabled);
-            match.setAttribute("name", curPattern.name);
+    for (j = 0, patternLen = patternsList.length; j < patternLen; j++) {
+      curPattern = patternsList[j].data;
+      match = xmlDoc.createElement("match");
+      match.setAttribute("enabled", curPattern.enabled);
+      match.setAttribute("name", curPattern.name);
 
-            if (curPattern.type != "wildcard") {
-                match.setAttribute("isRegex", true);
-                match.setAttribute("pattern", curPattern.regex);
-            } else {
-                match.setAttribute("isRegex", false);
-                match.setAttribute("pattern", curPattern.url);
-            }
+      if (curPattern.type != "wildcard") {
+        match.setAttribute("isRegex", true);
+      } else {
+        match.setAttribute("isRegex", false);
+      }
+      match.setAttribute("pattern", curPattern.url);
+      match.setAttribute("reload", true);
+      match.setAttribute("autoReload", false);
 
-            match.setAttribute("reload", true);
-            match.setAttribute("autoReload", false);
+      if (curPattern.whitelist == "Inclusive") {
+        match.setAttribute("isBlacklist", false);
+      } else {
+        match.setAttribute("isBlacklist", true);
+      }
 
-            if (curPattern.whitelist == "Inclusive") {
-                match.setAttribute("isBlacklist", false);
-            } else {
-                match.setAttribute("isBlacklist", true);
-            }
+      match.setAttribute("isMultiline", false);
+      match.setAttribute("fromSubscription", false);
+      match.setAttribute("caseSensitive", false);
 
-            match.setAttribute("isMultiline", false);
-            match.setAttribute("fromSubscription", false);
-            match.setAttribute("caseSensitive", false);
-
-            matches.appendChild(match);
-        }
-
-        return matches;
-
+      matches.appendChild(match);
     }
 
-    return null;
+    return matches;
+
+  }
+
+  return null;
+
+};
+
+var setDefaultFoxyProxySettings = function (foxyproxy, xmlDoc) {
+
+  var item;
+  
+  var defaultSettings = {
+    mode:"disabled", 
+    selectedTabIndex:"6", 
+    toolbaricon:"true", 
+    toolsMenu:"true", 
+    contextMenu:"true", 
+    advancedMenus:"false", 
+    previousMode:"disabled", 
+    resetIconColors:"true", 
+    useStatusBarPrefix:"true", 
+    excludePatternsFromCycling:"false", 
+    excludeDisabledFromCycling:"false", 
+    ignoreProxyScheme:"false", 
+    apiDisabled:"false", 
+    proxyForVersionCheck:""
+  };
+  for (item in defaultSettings) {
+    foxyproxy.setAttribute(item, defaultSettings[item]);
+  }
+
+  var random = xmlDoc.createElement("random");
+  random.setAttribute("includeDirect", false);
+  random.setAttribute("includeDisabled", false);
+  
+  var statusSettings = {
+    "icon": true,
+    "text": false,
+    "left": "options",
+    "middle": "cycle",
+    "right": "contextmenu",
+    "width": 0
+  };
+  var statusbar = xmlDoc.createElement("statusbar");
+  for (item in statusSettings) {
+    statusbar.setAttribute(item, statusSettings[item]);
+  }
+
+  var logg = xmlDoc.createElement("logg");
+  logg.setAttribute("enabled", false);
+  logg.setAttribute("maxSize", "500");
+  logg.setAttribute("noURLS", false);
+  logg.setAttribute("header", "&lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;&lt;!DOCTYPE html PUBLIC &quot;-//W3C//DTD XHTML 1.0 Strict//EN&quot; &quot;http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd&quot;&gt;&lt;html xmlns=&quot;http://www.w3.org/1999/xhtml&quot;&gt;&lt;head&gt;&lt;title&gt;&lt;/title&gt;&lt;link rel=&quot;icon&quot; href=&quot;http://getfoxyproxy.org/favicon.ico&quot;/&gt;&lt;link rel=&quot;shortcut icon&quot; href=&quot;http://getfoxyproxy.org/favicon.ico&quot;/&gt;&lt;link rel=&quot;stylesheet&quot; href=&quot;http://getfoxyproxy.org/styles/log.css&quot; type=&quot;text/css&quot;/&gt;&lt;/head&gt;&lt;body&gt;&lt;table class=&quot;log-table&quot;&gt;&lt;thead&gt;&lt;tr&gt;&lt;td class=&quot;heading&quot;&gt;${timestamp-heading}&lt;/td&gt;&lt;td class=&quot;heading&quot;&gt;${url-heading}&lt;/td&gt;&lt;td class=&quot;heading&quot;&gt;${proxy-name-heading}&lt;/td&gt;&lt;td class=&quot;heading&quot;&gt;${proxy-notes-heading}&lt;/td&gt;&lt;td class=&quot;heading&quot;&gt;${pattern-name-heading}&lt;/td&gt;&lt;td class=&quot;heading&quot;&gt;${pattern-heading}&lt;/td&gt;&lt;td class=&quot;heading&quot;&gt;${pattern-case-heading}&lt;/td&gt;&lt;td class=&quot;heading&quot;&gt;${pattern-type-heading}&lt;/td&gt;&lt;td class=&quot;heading&quot;&gt;${pattern-color-heading}&lt;/td&gt;&lt;td class=&quot;heading&quot;&gt;${pac-result-heading}&lt;/td&gt;&lt;td class=&quot;heading&quot;&gt;${error-msg-heading}&lt;/td&gt;&lt;/tr&gt;&lt;/thead&gt;&lt;tfoot&gt;&lt;tr&gt;&lt;td/&gt;&lt;/tr&gt;&lt;/tfoot&gt;&lt;tbody&gt;");
+  logg.setAttribute("row", "&lt;tr&gt;&lt;td class=&quot;timestamp&quot;&gt;${timestamp}&lt;/td&gt;&lt;td class=&quot;url&quot;&gt;&lt;a href=&quot;${url}&quot;&gt;${url}&lt;/a&gt;&lt;/td&gt;&lt;td class=&quot;proxy-name&quot;&gt;${proxy-name}&lt;/td&gt;&lt;td class=&quot;proxy-notes&quot;&gt;${proxy-notes}&lt;/td&gt;&lt;td class=&quot;pattern-name&quot;&gt;${pattern-name}&lt;/td&gt;&lt;td class=&quot;pattern&quot;&gt;${pattern}&lt;/td&gt;&lt;td class=&quot;pattern-case&quot;&gt;${pattern-case}&lt;/td&gt;&lt;td class=&quot;pattern-type&quot;&gt;${pattern-type}&lt;/td&gt;&lt;td class=&quot;pattern-color&quot;&gt;${pattern-color}&lt;/td&gt;&lt;td class=&quot;pac-result&quot;&gt;${pac-result}&lt;/td&gt;&lt;td class=&quot;error-msg&quot;&gt;${error-msg}&lt;/td&gt;&lt;/tr&gt;");
+  logg.setAttribute("footer", "lt;/tbody&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;");
+  
+  var autoSettings = {
+    "enabled": false,
+    "temp": false,
+    "reload": true,
+    "notify": true,
+    "notifyWhenCanceled": true,
+    "prompt": true
+  };
+  var autoadd = xmlDoc.createElement("autoadd");
+  for (item in autoSettings) {
+    autoadd.setAttribute(item, autoSettings[item]);
+  }
+
+  var quickadd = xmlDoc.createElement("quickadd");
+  quickadd.setAttribute("enabled", false);
+  quickadd.setAttribute("temp", false);
+  quickadd.setAttribute("reload", true);
+  quickadd.setAttribute("notify", true);
+  quickadd.setAttribute("notifyWhenCanceled", true);
+  quickadd.setAttribute("prompt", true);
+  
+  var defaultPrefs = xmlDoc.createElement("defaultPrefs");
+  defaultPrefs.setAttribute("origPrefetch", 0);  
+
+  foxyproxy.appendChild(random);
+  foxyproxy.appendChild(statusbar);
+  foxyproxy.appendChild(logg);
+  foxyproxy.appendChild(autoadd);
+  foxyproxy.appendChild(quickadd);
+  foxyproxy.appendChild(defaultPrefs);
+
+};
+
+var setSingleProxyNode = function (xmlDoc, curProxy) {
+
+  var curProxyNode = xmlDoc.createElement("proxy");
+  var manualconf, autoconf, patternsNode, warnings;
+
+  // general settings.
+  curProxyNode.setAttribute('name', curProxy.name);
+  curProxyNode.setAttribute('id', curProxy.id);
+  curProxyNode.setAttribute("notes", curProxy.notes);
+  curProxyNode.setAttribute('enabled', curProxy.enabled);
+  curProxyNode.setAttribute('color', curProxy.color);
+  curProxyNode.setAttribute('isSocks', curProxy.isSocks);
+  curProxyNode.setAttribute("mode", curProxy.type);
+  curProxyNode.setAttribute("autoconfMode", "pac");
+  
+  // manual conf
+  manualconf = xmlDoc.createElement("manualconf");
+  manualconf.setAttribute("host", curProxy.host);
+  manualconf.setAttribute("port", curProxy.port);
+  manualconf.setAttribute("socksversion", curProxy.socks);
+  curProxyNode.appendChild(manualconf);
+
+  // autoconf
+  autoconf = xmlDoc.createElement("autoconf");
+  autoconf.setAttribute("url", curProxy.configUrl);
+  autoconf.setAttribute("reloadPac", false);
+  autoconf.setAttribute("loadNotification", true);
+  autoconf.setAttribute("errorNotification", true);
+  autoconf.setAttribute("autoReload", false);
+  autoconf.setAttribute("reloadFreqMins", "60");
+  autoconf.setAttribute("disableOnBadPAC", true);
+  curProxyNode.appendChild(autoconf);
+  
+  warnings = xmlDoc.createElement("warnings");
+  warnings.setAttribute("white-patterns", true);
+  curProxyNode.appendChild(warnings);
+
+  if (curProxy.patterns) {
+    patternsNode = generateXMLForPatterns(xmlDoc, curProxy.patterns);
+    if (patternsNode) {
+      curProxyNode.appendChild(patternsNode);
+    }
+  }
+
+  return curProxyNode;
 
 };
 
 var generateXMLFromStorage = function () {
-    var xmlDoc = document.implementation.createDocument(null, null, null);
-    var foxyproxy = xmlDoc.createElement("foxyproxy");
+  var xmlDoc = document.implementation.createDocument(null, null, null);
+  var foxyproxy = xmlDoc.createElement("foxyproxy");
 
-    var proxies = xmlDoc.createElement("proxies");
+  // for Firefox compatibility.
+  setDefaultFoxyProxySettings(foxyproxy, xmlDoc);
 
-    var proxyList = JSON.parse(localStorage.getItem("proxyList")),
-        len = proxyList.length, i = 0, curProxy, curProxyNode, 
-        attr, manualconf, patterns, patternsNode;
-    
-    for (; i < len; i++) {
-        curProxy = proxyList[i].data;
+  var proxies = xmlDoc.createElement("proxies");
 
-        if (curProxy.id == 'default') {
-            continue;
-        }
-
-        curProxyNode = xmlDoc.createElement("proxy");
-        
-        curProxyNode.setAttribute('name', curProxy.name);
-        curProxyNode.setAttribute('id', curProxy.id);
-        curProxyNode.setAttribute('enabled', curProxy.enabled);
-        curProxyNode.setAttribute('color', curProxy.color);
-        curProxyNode.setAttribute('isSocks', curProxy.isSocks);
-        curProxyNode.setAttribute("mode", curProxy.type);
-
-        if (curProxy.type == "manual") {
-            manualconf = xmlDoc.createElement("manualconf");
-            manualconf.setAttribute("host", curProxy.host);
-            manualconf.setAttribute("port", curProxy.port);
-            curProxyNode.appendChild(manualconf);
-        }
-
-
-        console.log(curProxy.patterns);
-        if (curProxy.patterns) {
-            patternsNode = generateXMLForPatterns(xmlDoc, curProxy.patterns);
-            if (patternsNode) {
-                curProxyNode.appendChild(patternsNode);
-            }
-        }
-        
-
-        proxies.appendChild(curProxyNode);
-        
-    }
-    
-    xmlDoc.appendChild(foxyproxy);
-    foxyproxy.appendChild(proxies);
-    
-    console.log(xmlDoc);
+  var proxyList = JSON.parse(localStorage.getItem("proxyList")),
+      len = proxyList.length, i = 0, curProxy, curProxyNode, 
+      attr, manualconf, patterns, patternsNode, autoconf;
+  
+  for (; i < len; i++) {
+    curProxy = proxyList[i].data;
+    curProxyNode = setSingleProxyNode(xmlDoc, curProxy);
+    proxies.appendChild(curProxyNode);
+  }
+  
+  xmlDoc.appendChild(foxyproxy);
+  foxyproxy.appendChild(proxies);
+  
+  return xmlDoc;
 };
 
 var exportAndDownload = function() {
-    var blob = new Blob(['<?xml version="1.0" encoding="UTF-8"?><foxyproxy><\/foxyproxy>']);
-    var evt = document.createEvent("HTMLEvents");
-    /*
-    evt.initEvent("click");
-    $("<a>", {
-        download: "FoxyProxy-export.fpx",
-        href: webkitURL.createObjectURL(blob)
-    }).get(0).dispatchEvent(evt);
-     */
-    generateXMLFromStorage();
+  var dom = generateXMLFromStorage();
+  var xmlString = new XMLSerializer().serializeToString(dom);
+  var blob = new Blob(['<?xml version="1.0" encoding="UTF-8"?>' + xmlString]);
+  var evt = document.createEvent("HTMLEvents");
+  
+   evt.initEvent("click");
+   $("<a>", {
+   download: "FoxyProxy-export.fpx",
+   href: webkitURL.createObjectURL(blob)
+   }).get(0).dispatchEvent(evt);
+   
+
 };
 
 $(document).ready(function (e) {
 
-    var $button = $("#export-to-file");
-    $button.click(function (e) {
-        exportAndDownload();
-    });
+  var $button = $("#export-to-file");
+  $button.click(function (e) {
+    exportAndDownload();
+  });
 
 }); 
