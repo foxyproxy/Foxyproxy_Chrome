02e794e4cf9b7cdd6fb0b104f5f63a8f1382622c
M. Collins <collins.mike@gmail.com>
2014-08-15 15:45:05 -0600

    add a new url to open a window for first time installers

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var first_time_url = "https://getfoxyproxy.org/proxyservice/2.0/index.html";
+
 /* onInstalled listener opens tab to the appropriate post-install page. */
 chrome.runtime.onInstalled.addListener(function(details) {
     var urlToOpen;
@@ -9,12 +11,27 @@ chrome.runtime.onInstalled.addListener(function(details) {
             edition = chrome.i18n.getMessage("FoxyProxy_Edition").toLowerCase(),
             version = chrome.i18n.getMessage("FoxyProxy_Version").split('.');
         
-        if ("dev" == edition || version.length > 3) {
+        if ("dev" == edition) {
+            // chrome.windows.create({
+            //     "url": first_time_url,
+            //     "width": 999,
+            //     "height": 777,
+            //     "focused": true,
+            //     "type": "normal"
+            // });
             return; //short-circuit to avoid opening tabs in dev mode.
         }
         
         if (details.reason == "install") {
             urlToOpen = "http://getfoxyproxy.org/" + target + "/" + edition + "/install.html";
+            
+            chrome.windows.create({
+                "url": first_time_url,
+                "width": 999,
+                "height": 777,
+                "focused": true,
+                "type": "normal"
+            });
         } else if (details.reason == "update") {
             urlToOpen = "http://getfoxyproxy.org/" + target + "/" + edition + "/update.html";
         }
